aed0fa66fc18fb689de6fa5685e221925be357de
Tigran200630 <tigrandayanov2006@gmail.com>
2024-12-19 10:15:31 +0300

    Update task1.c

--- 3824Б1ПР5/06 Даянов Тигран Денисович/task1.c
+++ 3824Б1ПР5/06 Даянов Тигран Денисович/task1.c
@@ -4,3 +4,21 @@ size_t task1(int A[], size_t n)
 {
   return -1;
 }
+
+#include <stdio.h>
+
+// Функция для подсчёта количества чисел, меньших своего правого соседа
+size_t task1(int A[], size_t n) {
+    // Если массив меньше чем два элемента, результат очевиден
+    if (n < 2) {
+        return 0;
+    }
+
+    size_t count = 0; // Счётчик чисел, удовлетворяющих условию
+    for (size_t i = 0; i < n - 1; i++) {
+        if (A[i] < A[i + 1]) {
+            count++;
+        }
+    }
+    return count;
+}
